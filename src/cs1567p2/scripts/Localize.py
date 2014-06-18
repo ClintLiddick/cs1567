d70ef8956c741ef509021f80e5af3fe54234866e
Clint Liddick <cwl12@pitt.edu>
2014-06-18 16:41:20 -0400

    fixed mask code

--- src/cs1567p2/scripts/Localize.py
+++ src/cs1567p2/scripts/Localize.py
@@ -12,8 +12,8 @@ DPINK_PI = [175,120,230]
 RED_PA = [110,90,230]
 BLUE_PI = [200,205,175]
 
-color_mask_list = [RED_PA,BLUE_PI] # try to use Blue postit as position and Red paper as direction
-threshold = 20
+color_mask_list = [RED_PA] # try to use Blue postit as position and Red paper as direction
+threshold = 70
 locpub = None
 kinect1pub = None
 kinect2pub = None
@@ -22,6 +22,27 @@ top_mask = Image()
 mid_mask = Image()
 bot_mask = Image()
 
+def mask_image(message):
+    byte_array = list(message.data) #convert unit8[] from string to chars
+    for index in xrange(message.height*message.width): #iterate through
+        for k in xrange(len(color_mask_list)): 
+            #iterate through color list, if the bytes match, save the color
+            #in the mask
+            # ord(b) returns the value of ascii char (or 8-bit byte) b
+            if abs(color_mask_list[k][0] - ord(byte_array[3*index])) < threshold\
+                    and abs(color_mask_list[k][1] - ord(byte_array[3*index+1])) < threshold\
+                    and abs(color_mask_list[k][2] - ord(byte_array[3*index+2])) < threshold:
+                byte_array[3*index+0] = chr(color_mask_list[k][0])
+                byte_array[3*index+1] = chr(color_mask_list[k][1])
+                byte_array[3*index+2] = chr(color_mask_list[k][2])
+            else:
+                # chr(i) returns a byte ("ascii") whose value is i
+                byte_array[3*index] = chr(0) # each pixel = 3 bytes, 1 for each color BGR
+                byte_array[3*index+1] = chr(0)
+                byte_array[3*index+2] = chr(0)
+          
+    return "".join(byte_array) #make char[] back into uint8[] string
+ 
 def top_image_callback(message):
     global color_mask_list
     global top_mask
@@ -34,25 +55,10 @@ def top_image_callback(message):
     top_mask.encoding = message.encoding
     top_mask.is_bigendian = message.is_bigendian
     top_mask.step = message.step
+    mask = ""
     if message.encoding == "bgr8": #this is image_color encoding
-        byte_array = list(message.data) #convert unit8[] from string to chars
-        for index in xrange(message.height*message.width): #iterate through
-            # set pixel to black first
-            # chr(i) returns a byte ("ascii") whose value is i
-            byte_array[3*index] = chr(0) # each pixel = 3 bytes, 1 for each color BGR
-            byte_array[3*index+1] = chr(0)
-            byte_array[3*index+2] = chr(0)
-            for k in xrange(len(color_mask_list)): 
-                #iterate through color list, if the bytes match, save the color
-                #in the mask
-                # ord(b) returns the value of ascii char (or 8-bit byte) b
-                if abs(color_mask_list[k][0] - ord(byte_array[3*index])) < threshold\
-                        and abs(color_mask_list[k][1] - ord(byte_array[3*index+1])) < threshold\
-                        and abs(color_mask_list[k][2] - ord(byte_array[3*index+2])) < threshold:
-                    byte_array[3*index+0] = chr(color_mask_list[k][0])
-                    byte_array[3*index+1] = chr(color_mask_list[k][1])
-                    byte_array[3*index+2] = chr(color_mask_list[k][2])
-    top_mask.data = "".join(byte_array) #make char[] back into uint8[] string
+        mask = mask_image(message)
+    top_mask.data = mask
     kinect1pub.publish(top_mask) #publish the mask for viewing
     print "done1"
         
@@ -67,20 +73,10 @@ def mid_image_callback(message):
     mid_mask.encoding = message.encoding
     mid_mask.is_bigendian = message.is_bigendian
     mid_mask.step = message.step
+    mask = ""
     if message.encoding == "bgr8":
-        byte_array = list(message.data)
-        for index in xrange(message.height*message.width):
-            byte_array[3*index] = chr(0)
-            byte_array[3*index+1] = chr(0)
-            byte_array[3*index+2] = chr(0)
-            for k in xrange(len(color_mask_list)):
-                if abs(color_mask_list[k][0] - ord(byte_array[3*index])) < threshold\
-                        and abs(color_mask_list[k][1] - ord(byte_array[3*index+1])) < threshold\
-                        and abs(color_mask_list[k][2] - ord(byte_array[3*index+2])) < threshold:
-                    byte_array[3*index+0] = chr(color_mask_list[k][0])
-                    byte_array[3*index+1] = chr(color_mask_list[k][1])
-                    byte_array[3*index+2] = chr(color_mask_list[k][2])
-    mid_mask.data = "".join(byte_array)
+        mask = mask_image(message)
+    mid_mask.data = mask
     kinect3pub.publish(mid_mask)
     print "done3"
 
@@ -95,20 +91,10 @@ def bot_image_callback(message):
     bot_mask.encoding = message.encoding
     bot_mask.is_bigendian = message.is_bigendian
     bot_mask.step = message.step
-    if message.encoding == "bgr8":
-        byte_array = list(message.data)
-        for index in xrange(message.height*message.width):
-            byte_array[3*index] = chr(0)
-            byte_array[3*index+1] = chr(0)
-            byte_array[3*index+2] = chr(0)
-            for k in xrange(len(color_mask_list)):
-                if abs(color_mask_list[k][0] - ord(byte_array[3*index])) < threshold\
-                        and abs(color_mask_list[k][1] - ord(byte_array[3*index+1])) < threshold\
-                        and abs(color_mask_list[k][2] - ord(byte_array[3*index+2])) < threshold:
-                    byte_array[3*index+0] = chr(color_mask_list[k][0])
-                    byte_array[3*index+1] = chr(color_mask_list[k][1])
-                    byte_array[3*index+2] = chr(color_mask_list[k][2])
-    bot_mask.data = "".join(byte_array)
+    mask = ""
+    if message.encoding == "bgr8": #this is image_color encoding
+        mask = mask_image(message)
+    top_mask.data = mask
     kinect2pub.publish(bot_mask)
     print "done2"
 
@@ -118,7 +104,7 @@ def top_cloud_callback(message):
     try:
         #make a generator, skipping points that have no depth, on points in 
         # list of uvs (index into image [col,row]) or if empty list, get all pt
-        data_out = pc2.read_points(message, field_names=None, skip_nans=True, uvs=[]) 
+        data_out = pc2.read_points(message, field_names=None, skip_nans=True, uvs=[]) # list of 4-tuples
         i=0
         iteration1 = next(data_out) #format x,y,z,rgba
         while iteration1 != None:
@@ -153,9 +139,10 @@ def bot_cloud_callback(message):
 ###############################################
 # TODO
 
-def find_center(points):
+def find_center(points,color):
     # return point that is average of greatest/least y and x values, points::uint8[]
     print "find center"
+
 
 
 def find_center_near(points, loc, dist):
